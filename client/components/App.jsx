13620b1052bd15a3681c953b9f579b3cbc9f04d7
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-04-24 10:13:12 +1200

    add Card to App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Header from './Header'
-
+import Card from './Card'
 const App = () => {
 	return (
 		<>
 			<Header />
-			<h1>React development has begun!</h1>
+			<Card />
 		</>
 	)
 }
